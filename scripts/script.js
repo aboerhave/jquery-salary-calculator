c083300f8ef95d8a884a538863556ea2fb491999
Adam Boerhave <arboerhave@gmail.com>
2020-10-02 20:15:17 -0500

    submit fn started - logs ok on click

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,10 @@
 $(document).ready(onReady);
 
 function onReady() {
+    $('#submitBtn').on('click', submitBtnFunction);
+}
+
+function submitBtnFunction() {
+    console.log('submit button clicked');
     
 }
