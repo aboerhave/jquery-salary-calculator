9ab6b851b7643a4f3a9eececd89d260bf3b1720b
Adam Boerhave <arboerhave@gmail.com>
2020-10-04 15:14:45 -0500

    comments through js

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@
 let employeeData = [];  // empty array to store employee information
 let yearlyTotal = 0;    // variable to store yearly salary total
 let monthlyTotal = 0;   // variable to store monthly salaray total
-//let monthlytotal;  I think extra line to be removed
+let monthlyTotalString;  // variable to save string to display
 
 $(document).ready(onReady);
 
@@ -147,97 +147,73 @@ function appendToDom() {
 // deleteBtnFunction 
 function deleteBtnFunction() {
 console.log('delete button clicked');
+    // this line traverses the DOM to find the ID number for the entry to be deleted
     let removeId = $(this).parent().prev().prev().prev().text();
-    console.log('removeId', removeId);
+    // this line traverses the DOM to find the value for salary to be removed
     let removeSalary = $(this).parent().prev().text();
-    console.log('removeSalary', removeSalary);
+    // takes out symbols from string to use as a number
     let removeSalaryNumber = removeSalary.replace(",", "").replace('$',"");
+    // removeEntry called here.  It adjusts yearly and monthly salary values, 
+    // and removes the employee object from the employeeData array
     removeEntry(removeId, removeSalaryNumber);
+    // rest of table removed if only one entry remains
     if ($('table tr').length == 3) {
         $('#monthlyTotal').empty();
         $('#output').empty();
         $('#sectionHeading').text(`Employee Data Will Be Displayed Here Once Entered`);
     }
-    else {
+    else {  // one line is removed if there are multiple lines to the table
         $(this).parent().parent().remove();    
+        // monthyly total value changed on DOM
         $('#monthlyTotal').text(`Monthly Salary Total: $${monthlyTotalString}`);
     }
+    // changes color of background of text if needed
     checkColor();
-}
+}   // end deleteBtnFunction fn
 
+// removeEntry function accepts id number and salary for object to remove, and recalculates 
+// the yearlyTotal and monthlyTotal
 function removeEntry(removeId, removeSalary) {
-    console.log('yearlyTotal', yearlyTotal);
+    // adjusts yearly and monthly salary values
     yearlyTotal -= removeSalary;
-    console.log('new yearlyTotal');
-    
     monthlyTotal = yearlyTotal / 12;
+    // string to display with $ and , to 2 dec places
     monthlyTotalString = monthlyTotal.toLocaleString('en-US', {maximumFractionDigits:2});
-    console.log('yearlyTotal', yearlyTotal);
-    console.log('montylyTotal', monthlyTotal);
+    // searches array for entry to remove
     for (let i = 0; i < employeeData.length; i++) {
         // find index of entry to remove
         if (removeId == employeeData[i].idNumber) {
             employeeData.splice(i, 1);
         }
     }
-    
-}
+}// end removeEntry fn
 
-// may have been thinking about this wrong.  I think it's more than needed for regular,
-// but should chanbe properly if I get the remove to work
+// checkColor function reads value of monthlyTotal and changes the background of the text to 
+// red if it's higher than 20k, otherwise white
 function checkColor() {
     // the output line with monthly total is id=monthlyTotal
+    // it checks the new value and the class it currently has to see if it needs to change
     console.log($('#monthlyTotal').hasClass("highMonthlySalary")); 
     // new monthly total lower than 20k and previous assignment was to low class
     
     if (monthlyTotal <= 20000 && $('#monthlyTotal').hasClass("lowMonthlySalary")) {
         // leave same
-        console.log('case 1');
-        
     }
     // new monthly total <= 20k and previous assignment was to high class
     else if (monthlyTotal <= 20000 && $('#monthlyTotal').hasClass("highMonthlySalary")) {
         // change class to low
-        console.log('case 2');
         $('#monthlyTotal').removeClass("highMonthlySalary");
         $('#monthlyTotal').addClass("lowMonthlySalary");
     }
     // new monthly total > 20k and previous assignment was to low class
     else if (monthlyTotal > 20000 && $('#monthlyTotal').hasClass("lowMonthlySalary")) {
-        console.log('case 3');
         $('#monthlyTotal').removeClass("lowMonthlySalary");
         $('#monthlyTotal').addClass("highMonthlySalary");
     }
     // new monthly total > 20k and previous assignment was to high class
     else if (monthlyTotal > 20000 && $('#monthlyTotal').hasClass("highMonthlySalary")){
         // leave same
-        console.log('case 4');
-        
     }
-    
-
 }   // end checkColor fn
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* extra
-can use data function
-$.data("#myDiv").get(0),"key", "arbitrary value";
-
-data method:
-let myObj = {};
-$(myObj).data("city","Springfield");*/
